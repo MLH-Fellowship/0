5498cbaa309e8702e3ff88c78986375f62ccc9ab
Mwiza Simbeye <mwiza@agripredict.com>
2020-06-02 20:25:02 +0200

    added download_model.py to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 python -m venv env
-source $1/bin/activate
+source env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+echo "Downloading model..."
+python download_model.py
 
