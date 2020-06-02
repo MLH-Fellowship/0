31072b2f81e593b0926fa5f9fd17cfd0dc7ec3b1
Cesare De Cal <hi@cesare.io>
2020-06-02 17:29:15 +0200

    Created Flask project with basic Home and About templates, imported Boostrap for styling

---
+++ flask_project/flaskproject.py
@@ -0,0 +1,22 @@
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '93590f0e3dc7099d1f75f2607779ad4f'
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+# This condition is only true if we run the script directly
+# This helps avoid using environment variables
+# This way you can run the app by using 'python flaskproject.py'
+if __name__ == '__main__':
+    app.run(debug=True)
