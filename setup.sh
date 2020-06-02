bc9d86245b5a24e1fff04571303f3116da899d74
Mwiza Simbeye <mwiza@agripredict.com>
2020-06-02 20:39:41 +0200

    fixed numpy warnings for FutureWarning: Passing (type, 1) or '1type' as a synonym of type is deprecated

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ source env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 echo "Downloading model..."
-python download_model.py
-
+python download_model.py 124M
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
