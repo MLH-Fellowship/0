3e25a3e02bc49cc6a8d229576b29dd811a13d763
Cesare De Cal <hi@cesare.io>
2020-06-03 09:09:41 +0200

    Created wtforms to enable input validation and sanitization

--- flask_project/forms.py
+++ flask_project/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import URLField, StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import TextAreaField, SubmitField
+from wtforms.fields.html5 import URLField
+from wtforms.validators import DataRequired, URL, Length
 
 
 class SubmitWebsiteForm(FlaskForm):
-    websiteUrl = URLField('Website', validators=[DataRequired(), URL()])
+    websiteUrl = URLField('Summarize from Website',
+                          validators=[DataRequired(), URL()], description="test")
+    submit = SubmitField('Go')
+
+
+class SubmitTextForm(FlaskForm):
+    text = TextAreaField('Text', validators=[
+        DataRequired(), Length(min=1)])
     submit = SubmitField('Go')
