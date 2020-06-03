d85822a71c3700579cad5c9aea7351012165e802
kbanc <katherine.bancoft@gmail.com>
2020-06-03 14:08:37 -0400

    Added in function to create sentence vectors based on word occurance like in https://nlpforhackers.io/textrank-text-summarization/

--- flask_project/cosine_distance_summarizer.py
+++ flask_project/cosine_distance_summarizer.py
@@ -8,7 +8,7 @@ class CosineDistanceSummarizer(object):
     def get_summary(self, text, reduction_percent):
         doc = nlp(text.lower())
         sentences = list(doc.sents)
-        num_sentences = int(round(len(sentences)/(1-reduction_percent)))
+        num_sentences = int(round(len(sentences)*(1-reduction_percent)))
 
         matrix = self._create_cosine_similarity_matrix(sentences)
         ranking = np.argsort(np.sum(matrix, axis=0))
@@ -18,16 +18,38 @@ class CosineDistanceSummarizer(object):
             summary += str(sentences[ranking[i]])
         return summary
 
-
     def _create_cosine_similarity_matrix(self, sentences):
         numSentences = len(sentences)
         matrix = np.zeros((numSentences, numSentences))
         for i in range(numSentences):
             for j in range(numSentences):
                 if i != j: 
-                    matrix[i][j] = self._cosine_similarity(sentences[i], sentences[j])
+                    a = self._cosine_similarity_occurance(sentences[i], sentences[j])
+                    matrix[i][j] = a
         return matrix
 
-    def _cosine_similarity(self, sent1, sent2):
+    def _cosine_similarity_word2vec(self, sent1, sent2):
         if sent1.has_vector and sent2.has_vector:
             return sent1.similarity(sent2)
+    
+    def _cosine_similarity_occurance(self, sent1, sent2):
+        desiredPOS = ['PROPN', 'VERB', 'NOUN', 'ADJ']
+        all_words = []
+        for token in sent1:
+            if token.text not in all_words:
+                all_words.append(token.text)
+        for token in sent2:
+            if token.text not in all_words:
+                all_words.append(token.text)
+        vector1 = [0] * len(all_words)
+        vector2 = [0] * len(all_words)
+
+        for token in sent1:
+            if token.pos_ in desiredPOS:
+                vector1[all_words.index(token.text)] += 1
+    
+        for token in sent2:
+           if token.pos_ in desiredPOS:
+                vector2[all_words.index(token.text)] += 1
+    
+        return (np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
