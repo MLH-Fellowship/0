9668b09e0616542813eef4871cb209e6cd8c147d
Cesare De Cal <hi@cesare.io>
2020-06-04 10:21:02 +0200

    Added UI for summarization output, calculated reading time saved

--- flask_project/flaskproject.py
+++ flask_project/flaskproject.py
@@ -18,14 +18,39 @@ def home():
         text = text_form.text.data
         summary = get_summary(text)
         context = {'summary': summary}
-        return render_template('response.html', **context)
-    
+
+        # Calculate time saved by summary
+        minutes_before = estimate_reading_time(text)
+        minutes_after = estimate_reading_time(summary)
+        minutes_saved = round(minutes_before - minutes_after)
+
+        # Only show UI if time saved is substantial
+        if minutes_saved > 1:
+            context['minutes_saved'] = minutes_saved
+
+        return render_template('home.html', website_form=website_form, text_form=text_form, context=context)
+
     return render_template('home.html', website_form=website_form, text_form=text_form)
 
 
 @app.route('/summarize')
 def summarize():
     return 'Hello world'
+
+
+def estimate_reading_time(text):
+    # Based on research done in this field, people are able to read English at 200 WPM on paper,
+    # and 180 WPM on a monitor (the current record is 290 WPM).
+    WPM = 180
+    WORD_LENGTH = 5
+    total_words = count_words_in_text(text, WORD_LENGTH)
+    return total_words / WPM
+
+
+def count_words_in_text(text, word_length):
+    total_words = 0
+    total_words += len(text) / word_length
+    return total_words
 
 
 # This condition is only true if we run the script directly
